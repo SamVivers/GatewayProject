9fd13536296e7cdb6aab2bed5b87784bab3ecdba
SamVivers <49072095+SamVivers@users.noreply.github.com>
2019-06-05 13:11:48 +0100

    Update Login.vue

--- authentication-client/src/components/Login.vue
+++ authentication-client/src/components/Login.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
     <b-form class="mx-auto" @submit="submit">
-        <h1>Plz change plz</h1>
+        <h1>Please Login</h1>
         <b-form-input 
             v-model="form.email"
             type="email"
